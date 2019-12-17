429b4aa90474b5a79eb7a962b93dd8749924c08a
Oleh Sobchuk <oleg.sobchuk@gmail.com>
2019-12-17 23:47:08 +0200

    Amend seeds and docker-compose file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-admin = Admin.find_by(login: 'admin')
-
-unless admin
-  Admin.create(login: 'admin', password: '12345678', password_confirmation: '12345678', role: 'hight')
+if Admin.count < 1
+  Admin.create(
+    first_name: 'First',
+    last_name: 'Admin',
+    login: 'admin',
+    password: '12345678',
+    password_confirmation: '12345678',
+    role: 'hight'
+  )
 end
